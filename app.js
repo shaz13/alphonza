3aa6aefb1586a3f5f3341913021b528377bd2f23
Shaz <shahebaz13@gmail.com>
2017-06-19 05:22:34 +0530

    Dynamic PORT for heroku

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Alphonza')
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Alphonza server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
